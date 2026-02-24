a1e8da9a52a501dea96c2da809e5d73183e4ac04
74892rit <ritnarayanmallick22@gmail.com>
2026-02-24 16:30:13 +0530

    added some dependencies and utility function

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => {() => {}}
+// const asyncHandler = (fn) => () => {}
+
+// this is a try catch method to make a wrapper function for async await
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
